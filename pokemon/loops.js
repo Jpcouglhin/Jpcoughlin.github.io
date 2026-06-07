900d94dd88bfc70bf696b056c265b57f32139541
jpcoughlin <jpcoughlin2010@hotmail.com>
2026-06-07 16:50:27 -0400

    Add loops.js to log elements of a number list
    Fix typo in comment and log elements in reverse order.

---
+++ pokemon/loops.js
@@ -0,0 +1,9 @@
+// Creating a list pf ten numbers
+let my_list = [10,4,6,3,3,4,5,6,7,8,1,2]
+
+
+console.log(my_list[6])
+
+for(let Jpcoughlin= my_list.length; Jpcoughlin >= 0; Jpcoughlin--){
+    console.log(my_list[Jpcoughlin])
+}
