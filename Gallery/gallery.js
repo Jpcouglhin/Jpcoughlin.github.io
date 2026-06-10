7335b4d6deb28aaf783a86696bce1d75501d7d1a
jpcouglhin <jpcoughlin2010@hotmail.com>
2026-06-10 11:01:58 -0400

    Add image gallery functionality to gallery.js

--- Gallery/gallery.js
+++ Gallery/gallery.js
@@ -1,1 +1,17 @@
+// Example gallery.js
+const galleryImages = [
+    'images/photo1.jpg',
+    'images/photo2.jpg',
+    'images/photo3.jpg'
+];
+
+const galleryContainer = document.querySelector('.gallery-wrapper');
+
+galleryImages.forEach(src => {
+    const imgElement = document.createElement('img');
+    imgElement.src = src;
+    imgElement.alt = 'Gallery Image';
+    imgElement.classList.add('gallery-item');
+    galleryContainer.appendChild(imgElement);
+});
 
